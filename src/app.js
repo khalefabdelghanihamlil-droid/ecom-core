89f086b86d226a241faa1775d69fae6aca9cefbf
AbdelghaniHamlil <ghanouhamlil27@gmail.com>
2026-06-30 01:19:17 +0100

    ajout CORS et module finance

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const indexRoutes = require('./routes/index.routes');
 const webhookRoutes = require('./routes/webhook.routes');
 const otpRoutes = require('./routes/otp.routes');
@@ -7,6 +8,7 @@ const financeRoutes = require('./routes/finance.routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 app.use('/', indexRoutes);
