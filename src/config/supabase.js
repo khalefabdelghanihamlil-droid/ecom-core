323167ed9f1eef05ba437bc0500f974adb1f137e
AbdelghaniHamlil <ghanouhamlil27@gmail.com>
2026-07-08 14:24:51 +0100

    Configure Supabase WebSocket transport

--- src/config/supabase.js
+++ src/config/supabase.js
@@ -1,11 +1,19 @@
 const { createClient } = require('@supabase/supabase-js');
+const WebSocket = require('ws');
 const path = require('path');
 
-require('dotenv').config({ path: path.join(__dirname, '..', '..', '.env') });
+require('dotenv').config({
+  path: path.join(__dirname, '..', '..', '.env')
+});
 
 const supabase = createClient(
   process.env.SUPABASE_URL,
-  process.env.SUPABASE_KEY
+  process.env.SUPABASE_KEY,
+  {
+    realtime: {
+      transport: WebSocket
+    }
+  }
 );
 
 module.exports = supabase;
